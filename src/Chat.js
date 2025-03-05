fabd2a7aa52a957e6d565a24fec4f587c4eb3f2d
MM1918426 <marian.morcotet@oracle.com>
2025-03-05 19:18:19 +0200

    ui checkpoint

--- src/Chat.js
+++ src/Chat.js
@@ -1,19 +1,84 @@
 import { useState } from 'react';
 import { GrSend } from 'react-icons/gr';
+import Lottie from 'react-lottie';
+import animationData from './lotties/gift-on-the-way.json';
+import './heroSection.css';
 
 function Chat() {
   const [inputMessage, setInputMessage] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [agentResponse, setAgentResponse] = useState('');
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
+
   const n8nCall = (e) => {
     console.log('getGptResponse', e);
     console.log(e.target[0].value);
+    setLoading(true);
+    // fetch(
+    //   'https://trialplanner.app.n8n.cloud/webhook/b55a7a6b-e605-424f-af6a-c5429f6e045a',
+    //   {
+    //     method: 'POST',
+    //     body: JSON.stringify({ chatInput: e.target[0].value }),
+    //   }
+    // )
     fetch(
       'https://trialplanner.app.n8n.cloud/webhook-test/b55a7a6b-e605-424f-af6a-c5429f6e045a',
       {
         method: 'POST',
         body: JSON.stringify({ chatInput: e.target[0].value }),
       }
+    )
+      .then((response) => {
+        console.log('got data', response);
+        return response.text();
+      })
+      .then((data) => {
+        console.log('got data', data);
+        setAgentResponse(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setLoading(false);
+        console.log(err);
+      });
+  };
+
+  const HeroSection = () => {
+    return (
+      <div class="heroSectionContainer">
+        <section class="hero-section">
+          <div class="hero-container">
+            <h1 class="hero-title">
+              Transform Your Business
+              <br />
+              With Innovative Solutions
+            </h1>
+            <p class="hero-subtitle">
+              Discover cutting-edge strategies that drive growth, enhance
+              productivity, and unlock your company's full potential.
+            </p>
+            <div class="hero-buttons">
+              <a href="#" class="btn btn-primary">
+                Get Started
+              </a>
+              {/* <a href="#" class="btn btn-secondary">
+                Learn More
+              </a> */}
+            </div>
+          </div>
+        </section>
+      </div>
     );
   };
+
   return (
     <form
       onSubmit={(e) => {
@@ -23,6 +88,20 @@ function Chat() {
       class="inputContainer"
     >
       <div className="glass">
+        <div class="contentContainer">
+          {loading && <Lottie options={defaultOptions} />}
+          {!loading && <HeroSection />}
+        </div>
+
+        {/* <div style={{ position: 'relative' }}>
+          <div class="box">
+            <span>J</span>
+            <span>U</span>
+            <span>M</span>
+            <span>P</span>
+            <span>!</span>
+          </div>
+        </div> */}
         <input
           class="mainInput"
           type="text"
