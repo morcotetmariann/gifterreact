bd0de2b3a482766a35c1cf0a12da4cedc475489a
MM1918426 <marian.morcotet@oracle.com>
2025-03-05 13:47:48 +0200

    test payment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './Home';
-
+import { MdOutlineAccountCircle } from 'react-icons/md';
+import { useState } from 'react';
+// import Paylike from 'paylike';
 // App.js
 
 import { useAuth } from 'react-oidc-context';
@@ -9,11 +11,68 @@ import Chat from './Chat';
 
 function App() {
   const auth = useAuth();
+  const [dropdownState, setDropdownState] = useState(false);
+  const [dropdownValue, setDropdownValue] = useState('');
+  const key = '2ae0549d-4872-43fc-bc34-c4997c3b9ccd';
+  const paylike = window.Paylike({ key });
+
+  function pay() {
+    console.log('TRIED?!?!?');
+    paylike.pay(
+      {
+        // locale: 'da',  // pin popup to a locale
+        test: true,
+        title: 'Gifter App',
+        description: 'Discover Perfect Gifts with AI Magic!',
+
+        amount: { currency: 'EUR', exponent: 2, value: 19995 },
+
+        // saved on transaction for retrieval from dashboard or API
+        custom: {
+          // arrays are fine
+          products: [
+            // nested objects will do
+            { SKU: 'VMJ', quantity: 2 },
+            { SKU: 'UFB', quantity: 1 },
+          ],
+        },
+
+        // data from fields will be merged with custom
+        fields: [
+          // elaborate custom field
+          {
+            name: 'email',
+            type: 'email',
+            label: 'E-mail',
+            placeholder: 'user@example.com',
+            required: true,
+          },
+
+          // simple custom field
+          'note',
+        ],
+      },
+      (err, res) => {
+        if (err) return console.log(err);
+
+        console.log(res);
+        // location.href = '/thank-you'
+      }
+    );
+  }
+
+  const handleDropdownClick = () => {
+    setDropdownState(!dropdownState);
+  };
+  const handleSetDropdownValue = (value) => {
+    setDropdownValue(value);
+    setDropdownState(!dropdownState);
+  };
 
   const signOutRedirect = () => {
     const clientId = '3tu012q845tpuispds9jo24usf';
-    const logoutUri = '<logout uri>';
-    const cognitoDomain = 'https://<user pool domain>';
+    const logoutUri = '';
+    const cognitoDomain = 'https://us-east-1_6e0Jpb8aZ';
     window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(
       logoutUri
     )}`;
@@ -26,28 +85,99 @@ function App() {
   if (auth.error) {
     return <div>Encountering error... {auth.error.message}</div>;
   }
-
-  if (auth.isAuthenticated) {
-    return (
-      <div>
-        <Home></Home>
-        <Chat></Chat>
-        {/* <div>
-          <pre> Hello: {auth.user?.profile.email} </pre>
-          <pre> ID Token: {auth.user?.id_token} </pre>
-          <pre> Access Token: {auth.user?.access_token} </pre>
-          <pre> Refresh Token: {auth.user?.refresh_token} </pre>
-
-          <button onClick={() => auth.removeUser()}>Sign out</button>
-        </div> */}
-      </div>
-    );
-  }
-
+  console.log('WINDOW', window);
   return (
     <div>
-      <button onClick={() => auth.signinRedirect()}>Sign in</button>
-      <button onClick={() => signOutRedirect()}>Sign out</button>
+      <Home></Home>
+      <div className="main">
+        <div className="container">
+          <div className={`dropdown`}>
+            {auth.isAuthenticated ? (
+              <button
+                onClick={handleDropdownClick}
+                className="dropdown-btn transparentButton userEmail"
+              >
+                {auth.user?.profile.email.split('@')[0]}
+              </button>
+            ) : (
+              <button
+                onClick={handleDropdownClick}
+                className="dropdown-btn transparentButton rounded"
+              >
+                <MdOutlineAccountCircle
+                  size="2.5rem"
+                  style={{ color: 'hsl(262, 42%, 62%)' }}
+                />
+              </button>
+            )}
+            <div
+              className={`dropdown-items ${
+                dropdownState ? 'isVisible' : 'isHidden'
+              }`}
+            >
+              {auth.isAuthenticated ? (
+                <>
+                  <div className="dropdown-item">
+                    <button id="paylikePopup" onClick={() => pay()}>
+                      Pay
+                    </button>
+                    {/* <div
+                    className="dropdown__link"
+                    onClick={() => handleSetDropdownValue('value 02')}
+                  >
+                    Item 02
+                  </div> */}
+                  </div>
+                  <div className="dropdown-item">
+                    <button onClick={() => signOutRedirect()}>Sign out</button>
+                    {/* <div
+                    className="dropdown__link"
+                    onClick={() => handleSetDropdownValue('value 02')}
+                  >
+                    Item 02
+                  </div> */}
+                  </div>
+                </>
+              ) : (
+                <>
+                  <div className="dropdown-item">
+                    <button onClick={() => auth.signinRedirect()}>
+                      Sign in
+                    </button>
+                    {/* <div
+                    className="dropdown__link"
+                    onClick={() => handleSetDropdownValue('value 01')}
+                  >
+                    Item 01
+                  </div> */}
+                  </div>
+                  <div className="dropdown-item">
+                    <button onClick={() => signOutRedirect()}>Sign out</button>
+                    {/* <div
+                    className="dropdown__link"
+                    onClick={() => handleSetDropdownValue('value 02')}
+                  >
+                    Item 02
+                  </div> */}
+                  </div>
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+
+        <Chat></Chat>
+      </div>
+
+      {/* <Chat></Chat> */}
+      {/* <div>
+        <pre> Hello: {auth.user?.profile.email} </pre>
+        <pre> ID Token: {auth.user?.id_token} </pre>
+        <pre> Access Token: {auth.user?.access_token} </pre>
+        <pre> Refresh Token: {auth.user?.refresh_token} </pre>
+
+        <button onClick={() => auth.removeUser()}>Sign out</button>
+      </div> */}
     </div>
   );
 }
--- src/Chat.js
+++ src/Chat.js
@@ -15,32 +15,30 @@ function Chat() {
     );
   };
   return (
-    <div className="main">
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          n8nCall(e);
-        }}
-        class="inputContainer"
-      >
-        <div className="glass">
-          <input
-            class="mainInput"
-            type="text"
-            placeholder="Write the description of a friend or pet friend here"
-          ></input>
-          <div class="buttonContainer">
-            <button type="submit" class="inputButton">
-              <GrSend
-                style={{ color: 'hsl(262, 42%, 62%)' }}
-                size="2rem"
-              ></GrSend>
-              Send
-            </button>
-          </div>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        n8nCall(e);
+      }}
+      class="inputContainer"
+    >
+      <div className="glass">
+        <input
+          class="mainInput"
+          type="text"
+          placeholder="Write the description of a friend or pet friend here"
+        ></input>
+        <div class="buttonContainer">
+          <button type="submit" class="inputButton">
+            <GrSend
+              style={{ color: 'hsl(262, 42%, 62%)' }}
+              size="2rem"
+            ></GrSend>
+            Send
+          </button>
         </div>
-      </form>
-    </div>
+      </div>
+    </form>
   );
 }
 
