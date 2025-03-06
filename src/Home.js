dc10710cb1ee325d32e4abb3004e0eb50121a884
MM1918426 <marian.morcotet@oracle.com>
2025-03-06 11:44:33 +0200

    optimize production

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,10 @@
 import { useState } from 'react';
 import { useEffect, useRef } from 'react';
 import Spline from '@splinetool/react-spline';
-import ScenePreview from './Purple3dicons.jpg';
+import ScenePreview from './Purple3dicons_compressed60.jpg';
+// import ScenePreviewJPEG from './Purple3dicons_compressed80.jpeg';
+import ScenePreviewJPEG from './Purple3dicons_compressed80_compressed95.jpeg';
+import LocalScene from './purple_3_d_icons_copy.spline';
 
 function useWindowSize() {
   // Initialize state with undefined width/height so server and client renders match
@@ -42,10 +45,17 @@ function Home() {
   }, [size]);
   return (
     <div class="spline-container">
-      <img src={ScenePreview} alt="Loading..." class="placeholder"></img>
+      <img
+        // src="https://amplify-amplifyf77a3d68a4004-staging-122152-deployment.s3.us-east-1.amazonaws.com/images/Purple3dicons_compressed80_compressed95.jpeg"
+        src={ScenePreviewJPEG}
+        alt="Loading..."
+        class="placeholder"
+      ></img>
       <Spline
         class="preloadedBackground"
+        // scene="https://amplify-amplifyf77a3d68a4004-staging-122152-deployment.s3.us-east-1.amazonaws.com/images/purple_3_d_icons_copy.spline"
         scene="https://prod.spline.design/AURdrtpb7BhwChk4/scene.splinecode"
+        // scene={LocalScene}
         size={size}
       />
     </div>
